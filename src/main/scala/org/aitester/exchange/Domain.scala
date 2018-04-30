c37f25aaae6dd9ceb39550e5ccc6d2be97b673e0
tanujit.ghosh <tanujit.ghosh@gmail.com>
2018-04-30 06:31:55 -0500

    adding domain objects and logger add json4s serialization

--- src/main/scala/org/aitester/exchange/Domain.scala
+++ src/main/scala/org/aitester/exchange/Domain.scala
@@ -1,50 +1,82 @@
 package org.aitester.exchange
 
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, NoTypeHints}
 import org.json4s.JsonDSL.WithDouble._
 import org.json4s.native.JsonMethods._
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization._
 
 case class SecurityInfo(ticker: String, price : Double){
-  override def toString: String = s"securityInfo:${ticker}:${price}"
+  implicit val formats = Serialization.formats(NoTypeHints)
 
-  def asJson : String = compact(render( (( "ticker" -> ticker)
-    ~ ( "price" -> price)
-    )))
+  def asJson : String = write(this)
 }
+
 object SecurityInfo{
-  implicit val formats = DefaultFormats
+  implicit val formats = Serialization.formats(NoTypeHints)
 
-  def fromJson(jsonStr: String) : SecurityInfo = parse(jsonStr).extract[SecurityInfo]
+  val prefix = "SecurityInfo"
 
-  def apply(jsonStr: String) = fromJson(jsonStr)
+  def fromJson(jsonStr: String) : SecurityInfo = read[SecurityInfo](jsonStr)
 
-  def key(secInfo: SecurityInfo) : String = s"securityInfo:${secInfo.ticker}"
+  def apply(jsonStr: String) : SecurityInfo = fromJson(jsonStr)
 
-  def key(ticker: String) : String = s"securityInfo:${ticker}"
+  def key(secInfo: SecurityInfo) : String = key(secInfo.ticker)
+
+  def key(ticker: String) : String = s"${prefix}:${ticker}"
 }
 
 case class QuoteInfo(ticker:String, action: String, timestamp: Long, qty: Int, price: Double){
-  override def toString: String = s"quoteInfo:${ticker}:${action}:${timestamp}:${qty}:${price}"
+  implicit val formats = Serialization.formats(NoTypeHints)
 
-
-  def asJson : String = compact(render((
-            ("ticker" -> ticker)
-            ~ ( "action" -> action)
-            ~ ( "timestamp" -> timestamp)
-            ~ ( "qty" -> qty)
-            ~ ( "price" -> price )
-    )))
+  def asJson : String = write(this)
 }
 
 object QuoteInfo {
-  implicit val formats = DefaultFormats
+  implicit val formats = Serialization.formats(NoTypeHints)
+  val prefix = "QuoteInfo"
 
-  def fromJson(jsonStr: String) : QuoteInfo = parse(jsonStr).extract[QuoteInfo]
+  def fromJson(jsonStr: String) : QuoteInfo = read[QuoteInfo](jsonStr)
+
+  def apply(jsonStr : String) : QuoteInfo = fromJson(jsonStr)
+
+  def key(quoteInfo: QuoteInfo) : String = key(quoteInfo.ticker, quoteInfo.action)
+
+  def key(ticker: String, action: String) : String = s"${prefix}:${ticker}:${action}"
+}
+
+case class QuotesList(ticker:String, quoteList: Seq[QuoteInfo]){
+  implicit val formats = Serialization.formats(NoTypeHints)
+  def asJson : String = write(this)
+}
+
+object QuotesList {
+  implicit val formats = Serialization.formats(NoTypeHints)
+  val prefix = "QuotesList"
+
+  def fromJson(jsonStr: String) : QuotesList = read[QuotesList](jsonStr)
+
+  def apply(jsonStr:String): QuotesList = fromJson(jsonStr)
+
+  def key(quotesList: QuotesList) : String = key(quotesList.ticker)
+
+  def key(ticker: String) : String = s"${prefix}:${ticker}"
+}
+
+case class TradeInfo(ticker:String, action: String, timestamp: Long, qty: Int, price: Double){
+  implicit val formats = Serialization.formats(NoTypeHints)
+  def asJson : String = write(this)
+}
+
+object TradeInfo {
+  implicit val formats = Serialization.formats(NoTypeHints)
+  val prefix = "TradeInfo"
+
+  def fromJson(jsonStr: String) : TradeInfo = read[TradeInfo](jsonStr)
 
   def apply(jsonStr : String) = fromJson(jsonStr)
 
-  def key(quoteInfo: QuoteInfo) : String = s"quoteInfo:${quoteInfo.ticker}:${quoteInfo.action}"
+  def key(tradeInfo: TradeInfo) : String = key(tradeInfo.ticker, tradeInfo.action)
 
-  def key(ticker: String, action: String) : String = s"quoteInfo:${ticker}:${action}"
+  def key(ticker: String, action: String) : String = s"${prefix}:${ticker}:${action}"
 }
-
--- src/main/scala/org/aitester/exchange/Exchange.scala
+++ src/main/scala/org/aitester/exchange/Exchange.scala
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory
 import scala.io.StdIn
 
 
-//dummy class to simulate the exchange
+/**
+  * Class to simulate Exchange operations
+  *
+  */
 object Exchange extends App{
   val log = LoggerFactory.getLogger(Exchange.getClass.getName)
   implicit val system = ActorSystem("exchange")
@@ -43,20 +46,57 @@ object Exchange extends App{
   } ~ path ( "quote" / Segment / Segment / IntNumber / DoubleNumber) { (ticker, action, qty, price) =>
     {
       put {
+        //log the quote into db
         val quoteInfo = QuoteInfo(ticker, action, System.currentTimeMillis(), qty, price)
         log.info(s"QuoteInfo PUT : ${quoteInfo.asJson}")
         val key = QuoteInfo.key(ticker, action)
         levelDBStore.putInDB(key, quoteInfo.asJson)
+
+        //check with the list
+        val keyQuoteList = QuotesList.key(ticker)
+        val quotesListOption = levelDBStore.getFromDB(keyQuoteList)
+        quotesListOption match {
+          case Some(quotesListStr) => {
+            val quotesList = QuotesList.fromJson(quotesListStr)
+            //if we have less than 100 quotes keep adding to the list
+            if(quotesList.quoteList.size < 100){
+              val newQuotesList = QuotesList(quotesList.ticker, quotesList.quoteList ++ Seq(quoteInfo))
+              levelDBStore.putInDB(QuotesList.key(newQuotesList.ticker), newQuotesList.asJson)
+            } else {
+              //we adjust price when 100 quotes are reached
+              val newSecInfo = adjustPrice(quotesList)
+              levelDBStore.putInDB(SecurityInfo.key(newSecInfo), newSecInfo.asJson)
+              val newQuotesList = QuotesList(quotesList.ticker,Seq(quoteInfo))
+              levelDBStore.putInDB(QuotesList.key(newQuotesList), newQuotesList.asJson)
+            }
+          }
+          case None => {
+            val quotesList = QuotesList(ticker, Seq(quoteInfo))
+            levelDBStore.putInDB(QuotesList.key(quotesList.ticker), quotesList.asJson)
+          }
+        }
         complete(HttpEntity(ContentTypes.`application/json`, quoteInfo.asJson))
       }
     }
-  } ~ path ( "trade" / Segment / Segment / IntNumber / DoubleNumber) { (securityName, action, qty, price) =>
+  } ~ path ( "trade" / Segment / Segment / IntNumber / DoubleNumber) { (ticker, action, qty, price) =>
     {
-      put{
-        log.info(s"${securityName} : ${action} : ${qty} : ${price}")
+      put {
+        log.info(s"${ticker} : ${action} : ${qty} : ${price}")
         //if incoming price for buy is 1 tick better than current price - perform trade
         //if incoming price for sell is 1 tick lesse than current price - perform trade
         complete("SUCCESS")
+      }
+    }
+  }~ path( "quoteslist" / Segment ) { (ticker) =>
+    {
+      get {
+        log.info(s"QuotesList GET : ${ticker}")
+        val keyQuoteList = QuotesList.key(ticker)
+        val quotesListOption = levelDBStore.getFromDB(keyQuoteList)
+        quotesListOption match {
+          case Some(quotesListStr) => complete(HttpEntity(ContentTypes.`application/json`, quotesListStr))
+          case None => complete(HttpResponse(StatusCodes.BadRequest))
+        }
       }
     }
   }
@@ -66,4 +106,12 @@ object Exchange extends App{
   log.info("server running on port 8080. Press return to stop.")
   StdIn.readLine()
   bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+
+
+  def adjustPrice(quotesList: QuotesList) : SecurityInfo = {
+    //price adjustment logic
+    val sum = quotesList.quoteList.map(q => q.price * q.qty).reduce( (x,y) => x+y)
+    val newPrice = sum / quotesList.quoteList.size
+    SecurityInfo(quotesList.ticker, newPrice)
+  }
 }
